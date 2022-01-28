7f3f71517e61c1efaf1c69835ff6d92b2355c1b0
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2022-01-29 01:11:47 +0530

    health-issues updated

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const bubbles = document.querySelectorAll(".bubble")
+// // const bubbleNames = document.querySelectorAll(".bubble-content")
+// const bubbleNames =document.getElementsByClassName("buble-content")
+// const bubbles = document.querySelectorAll(".bubble")
+// const close = document.querySelector(".popup-button")
+// const popups = document.querySelectorAll(".popup")
+
+
 
 // bubbles.addEventListener("click",()=>{
 //     bubbles.forEach(bubble =>{
@@ -6,8 +12,19 @@ const bubbles = document.querySelectorAll(".bubble")
 //     })
 // })
 
+// bubbleNames.forEach(bubbleName =>{
+//     bubbleName.addEventListener("click",()=>{
+//         bubbles.forEach(bubble =>{
+//             bubble.classList.add("pop")
+//         })
+
+//     })
+// })
+
+const bubbles = document.querySelectorAll(".bubble")
+
 bubbles.forEach(bubble =>{
     bubble.addEventListener("click",()=>{
-        bubble.classList.add("pop")
+        bubble.classList.toggle("pop")
     })
 })
