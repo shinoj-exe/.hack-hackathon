8ec9163e0cdb12371c850268bb7bbbcc032af122
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2022-01-28 22:26:42 +0530

    issues section

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const bubble = document.querySelector(".bubble")
+const bubbles = document.querySelectorAll(".bubble")
 
+// bubbles.addEventListener("click",()=>{
+//     bubbles.forEach(bubble =>{
+//         bubble.classList.add("pop")
+//     })
+// })
 
-
-bubble.addEventListener("click",()=>{
-    bubble.classList.add("pop" )
+bubbles.forEach(bubble =>{
+    bubble.addEventListener("click",()=>{
+        bubble.classList.add("pop")
+    })
 })
