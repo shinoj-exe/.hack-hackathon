4816cd764822a262d9de2b3f21e73f62c5081cc7
tommathew01 <73937254+tommathew01@users.noreply.github.com>
2022-01-28 22:47:11 +0530

    change 2
    made all files 1

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const bubble = document.querySelector(".bubble")
 bubble.addEventListener("click",()=>{
     bubble.classList.add("pop" )
 })
+
+function yesnoCheck() {
+    if (document.getElementById('organisation').checked) {
+        document.getElementById('ifYes').style.visibility = 'visible';
+    }
+    else document.getElementById('ifYes').style.visibility = 'hidden';
+
+} 
--- team3/script/check.js.js
+++
@@ -1,7 +0,0 @@
-function yesnoCheck() {
-    if (document.getElementById('organisation').checked) {
-        document.getElementById('ifYes').style.visibility = 'visible';
-    }
-    else document.getElementById('ifYes').style.visibility = 'hidden';
-
-} 
