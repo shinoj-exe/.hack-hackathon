bf4b091614f0110694210cade8ff3af857c69108
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2022-01-29 17:48:22 +0530

    new

--- form.js
+++
@@ -1,9 +0,0 @@
-const resultsList = document.getElementById('qsns')
-        new URLSearchParams(window.location.search).forEach((value,
-        name) => {
-            resultsList.append(`${name}: ${value}`)
-            resultsList.append(document.createElement('br'))
-           
-        })
- 
-
